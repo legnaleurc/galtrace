db5d8a9d80e37d01e5586a523dcc888d29f0e64a
Wei Cheng Pan <legnaleurc@gmail.com>
2012-04-01 05:25:38 +0800

    Add test cases for load.cgi.

--- kernel/tests.py
+++ kernel/tests.py
@@ -1,16 +1,52 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 
+import json
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SimpleTest( TestCase ):
+
+	def setUp( self ):
+		User.objects.create_user( username = 'alpha', password = 'alpha' )
+
+	def testGetLoad( self ):
+		"""
+		GET /load.cgi
+		"""
+		c = Client()
+		response = c.get( '/load.cgi' )
+		self.assertEqual( response.status_code, 405 )
+
+	def testLoadWithoutUser( self ):
+		"""
+		POST /load.cgi
+		Should redirect to /
+		"""
+		c = Client()
+		response = c.post( '/load.cgi' )
+		self.assertEqual( response.status_code, 302 )
+
+	def testLoadWithUserWithoutArgs( self ):
+		"""
+		"""
+		c = Client()
+		result = c.login( username = 'alpha', password = 'alpha' )
+		self.assertTrue( result )
+		response = c.post( '/load.cgi' )
+		self.assertEqual( response.status_code, 200 )
+		result = json.loads( response.content )
+		self.assertFalse( result['success'] )
+
+	def testLoadWithUserWithArgs( self ):
+		"""
+		"""
+		c = Client()
+		result = c.login( username = 'alpha', password = 'alpha' )
+		self.assertTrue( result )
+		response = c.post( '/load.cgi', {
+			'offset': 0,
+			'limit': 100,
+		} )
+		self.assertEqual( response.status_code, 200 )
+		result = json.loads( response.content )
+		self.assertTrue( result['success'] )
