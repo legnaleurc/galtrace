892622f4fcb91e9c9892748d7f3f5e5a44af2fe2
Wei Cheng Pan <legnaleurc@gmail.com>
2012-10-24 19:58:31 +0800

    Disable admin.

--- galtrace/urls.py
+++ galtrace/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns( '',
 	} ),
 
 	# url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
-	url( r'^admin/', include( admin.site.urls ) ),
+	# url( r'^admin/', include( admin.site.urls ) ),
 )
 
 from django.conf import settings
