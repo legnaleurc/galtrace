4b3476c18a116e2af506bcaa10303170bc356c15
Wei Cheng Pan <legnaleurc@gmail.com>
2012-11-05 12:34:44 +0800

    Fix favicon error.

--- galtrace/urls.py
+++ galtrace/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns( '',
+	url( r'^favicon\.ico|apple-touch-icon\.png$', RedirectView.as_view( url = '/static/img/favicon.ico' ) ),
+
 	url( r'', include( 'kernel.urls' ) ),
-	url( r'^favicon\.ico|apple-touch-icon\.png$', 'django.views.generic.simple.redirect_to', {
-		'url': '/static/img/favicon.ico',
-	} ),
 
 	# url( r'^admin/doc/', include( 'django.contrib.admindocs.urls' ) ),
 	# url( r'^admin/', include( admin.site.urls ) ),
