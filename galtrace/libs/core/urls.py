d7292cb05bde3a96c5f32cc4cbdb367f13b2a774
Wei Cheng Pan <legnaleurc@gmail.com>
2013-01-14 22:15:06 +0800

    Move url patterns.

--- galtrace/libs/core/urls.py
+++ galtrace/libs/core/urls.py
@@ -1,16 +1,7 @@
-import re
-
-from django.conf import settings
 from django.conf.urls import patterns, url
-from django.views.generic.base import RedirectView
 
 
-urlpatterns = patterns( '',
-	url( r'^favicon\.ico|apple-touch-icon\.png$', RedirectView.as_view( url = '/static/favicon.ico' ) ),
-	url( r'^robots\.txt$', RedirectView.as_view( url = '/static/robots.txt' ) ),
-)
-
-urlpatterns += patterns( 'galtrace.libs.core.views',
+urlpatterns = patterns( 'galtrace.libs.core.views',
 	url( r'^load\.cgi$', 'load' ),
 	url( r'^save\.cgi$', 'save' ),
 	url( r'^move\.cgi$', 'move' ),
@@ -18,11 +9,3 @@ urlpatterns += patterns( 'galtrace.libs.core.views',
 	url( r'^backup\.cgi$', 'backup' ),
 	url( r'^fetch\.cgi$', 'fetch' ),
 )
-
-if not settings.DEBUG:
-	urlpatterns[:0] = patterns( '',
-		url( r'^{0}(?P<path>.*)$'.format( re.escape( settings.STATIC_URL.lstrip( '/' ) ) ), 'django.views.static.serve', {
-			'document_root': settings.STATIC_ROOT,
-			'show_indexes': False,
-		} ),
-	)
--- galtrace/urls/production.py
+++ galtrace/urls/production.py
@@ -1,1 +1,16 @@
+import re
+
+from django.conf import settings
+from django.views.generic.base import RedirectView
+
 from . import *
+
+
+urlpatterns[:0] = patterns( '',
+	url( r'^{0}(?P<path>.*)$'.format( re.escape( settings.STATIC_URL.lstrip( '/' ) ) ), 'django.views.static.serve', {
+		'document_root': settings.STATIC_ROOT,
+		'show_indexes': False,
+	} ),
+	url( r'^favicon\.ico|apple-touch-icon\.png$', RedirectView.as_view( url = '/static/favicon.ico' ) ),
+	url( r'^robots\.txt$', RedirectView.as_view( url = '/static/robots.txt' ) ),
+)
