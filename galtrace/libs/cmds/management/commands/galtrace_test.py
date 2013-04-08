ad97c3b5568e41d4a408e092c8faeb4f54f16514
Wei Cheng Pan <legnaleurc@gmail.com>
2013-04-09 05:15:34 +0800

    Fix test command exit code.

--- galtrace/libs/cmds/management/commands/galtrace_test.py
+++ galtrace/libs/cmds/management/commands/galtrace_test.py
@@ -1,11 +1,15 @@
 import subprocess
+import sys
 
 from django.core.management.base import NoArgsCommand
+
 
 class Command( NoArgsCommand ):
 
 	help = 'only tests galtrace apps'
 
 	def handle_noargs( self, **options ):
-		subprocess.call( [ 'python', '-m', 'unittest', 'galtrace.libs.crawler.tests' ] )
-		subprocess.call( [ 'python', 'manage.py', 'test', 'core' ] )
+		a = subprocess.call( [ 'python', '-m', 'unittest', 'galtrace.libs.crawler.tests' ] )
+		b = subprocess.call( [ 'python', 'manage.py', 'test', 'core' ] )
+		if any( ( a, b ) ):
+			sys.exit( 1 )
