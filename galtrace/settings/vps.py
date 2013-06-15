1e8cbe55790b1fc7dccd55d16cf2fc8710044ed4
Wei Cheng Pan <legnaleurc@gmail.com>
2013-06-15 16:06:34 +0800

    Add VPS configuration.

---
+++ galtrace/settings/vps.py
@@ -0,0 +1,50 @@
+from os.path import join
+
+from . import *
+
+
+GALTRACE_DATABASE_PATH = join( GALTRACE_SETTINGS_ROOT, 'default.sqlite' )
+GALTRACE_SECRET = fromJsonFile()
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = GALTRACE_SECRET['ADMINS']
+
+MANAGERS = ADMINS
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': GALTRACE_DATABASE_PATH,
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': '',
+		'PORT': '',
+	},
+}
+
+SECRET_KEY = GALTRACE_SECRET['SECRET_KEY']
+
+ROOT_URLCONF = 'galtrace.urls.vps'
+
+# email settings
+EMAIL_BACKEND = 'galtrace.libs.mail.GmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = GALTRACE_SECRET['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = GALTRACE_SECRET['EMAIL_HOST_USER']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+GOOGLE_API_CLIENT_ID = GALTRACE_SECRET['GOOGLE_API_CLIENT_ID']
+GOOGLE_API_CLIENT_SECRET = GALTRACE_SECRET['GOOGLE_API_CLIENT_SECRET']
+SERVER_EMAIL = GALTRACE_SECRET['SERVER_EMAIL']
+
+# ssl settings
+ALLOWED_HOSTS = [
+	'galtrace.wcpan.info',
+]
+
+# static files settings
+STATIC_ROOT = GALTRACE_SECRET['STATIC_ROOT']
