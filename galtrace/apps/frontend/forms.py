4c41797fc30bc96021e1010d8443f62fcdae2340
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-02-24 11:48:05 +0800

    Fix JSON error.

--- galtrace/apps/frontend/forms.py
+++ galtrace/apps/frontend/forms.py
@@ -4,15 +4,19 @@ from django import forms
 
 from galtrace.libs.core.models import Order, PHASES
 
+
 class RestoreForm(forms.Form):
+
     data = forms.FileField()
 
     def save(self, user):
-        data_ = self.cleaned_data['data']
-        data_ = json.load(data_)
-        return Order.objects.restore(user, data_)
+        data = self.cleaned_data['data']
+        data = json.loads(data.read().decode())
+        return Order.objects.restore(user, data)
+
 
 class OrderForm(forms.ModelForm):
+
     class Meta:
         model = Order
         fields = ('uri', 'title', 'vendor', 'date', 'thumb', 'phase')
@@ -21,7 +25,9 @@ class OrderForm(forms.ModelForm):
             'phase': forms.Select(choices=PHASES),
         }
 
+
 class EditorForm(forms.Form):
+
     title = forms.CharField()
     vendor = forms.CharField()
     date = forms.CharField()
